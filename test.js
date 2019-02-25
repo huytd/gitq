cb1e026fafbba5f54206d66454ae7407a914229b
Huy <kingbazoka@gmail.com>
2019-02-25 14:51:45 -0800

    Initial commit

---
+++ test.js
@@ -0,0 +1,26 @@
+/*
+const test = s => {
+    console.log(s, buildQuery(s));
+};
+
+test("by huy");
+test("by huy tran");
+test("by huy@gmail.com");
+test("by huy and from 2019-01-01");
+test("by huy tran and from 2019-01-01");
+test("by huy@gmail.com and from 2019-01-01");
+test("by huy and from 2019-01-01 to 2019-01-20");
+test("by huy tran and from 2019-01-01 to 2019-01-20");
+test("by huy@gmail.com and from 2019-01-01 to 2019-01-20");
+test("by huy and to 2019-01-01 from 2019-01-20");
+test("by huy tran and to 2019-01-01 from 2019-01-20");
+test("by huy@gmail.com and to 2019-01-01 from 2019-01-20");
+test(" with other stuff came here");
+test("by huy with other stuff came here");
+test("by huy tran  with other stuff came here");
+test("by huy@gmail.com and other stuff came here");
+test("by huy and to 2019-01-01 from 2019-01-20 with some other thing");
+test("by huy tran and to 2019-01-01 from 2019-01-20 with other things");
+test("by huy@gmail.com and to 2019-01-01 from 2019-01-20 and Approved-by Huy");
+test("by huy@gmail.com and to 2019-01-01 from 2019-01-20 with Approved-by Huy");
+*/
